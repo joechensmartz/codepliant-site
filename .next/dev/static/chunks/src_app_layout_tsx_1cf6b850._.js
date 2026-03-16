4a9b3630a7bd9d02da60dbc6523875b98ea9e750
Joe <joe@JoedeMacBook-Air.local>
2026-03-16 00:50:31 -0500

    test: round 8-9 repo tests complete

--- .next/dev/static/chunks/src_app_layout_tsx_1cf6b850._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__81cfc4bd._.css",
-  "static/chunks/node_modules_5bcd87a1._.js"
-],
-    source: "dynamic"
-});
